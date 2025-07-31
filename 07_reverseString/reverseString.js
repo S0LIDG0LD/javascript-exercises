36bca7f57b167a4cc002ab89e060994ca40c4ca7
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-07-31 16:47:48 +0100

    All 4 tests passed. Transformed the string in array reversed the array joined the array in a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = Array.from(str);
+    strArr.reverse();
+    return strArr.join("");
 };
 
 // Do not edit below this line
