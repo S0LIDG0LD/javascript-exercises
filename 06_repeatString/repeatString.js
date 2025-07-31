eccedf039e6e1fe49236bb553e3798f2294a4a75
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-07-31 16:41:52 +0100

    all 8 tests passed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+str = "hey"
+num = 3
 
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let finalStr = ""
+    for (i=0; i < num; i++) {
+        finalStr += str;
+    }
+    return finalStr
 };
 
 // Do not edit below this line
