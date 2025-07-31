9f0b2d2e81de2e118672a975522428efd2975cd1
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-07-31 17:44:47 +0100

    All 7 tests passed. 1 Tested all errors using number methods 2 Tested and swapped variabled between smaller and bigger 3 iterated from smaller to bigger number while adding the smaller number to the total

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || Number.isNaN(a) || Number.isNaN(b)) {
+        return "ERROR";
+    }
+    let total = 0;
+    if (a > b) {
+        [a, b] = [b, a]
+    }
+    for (i = a; i <= b; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
