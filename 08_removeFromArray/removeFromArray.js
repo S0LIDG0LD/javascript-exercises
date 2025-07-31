9c56ed53a641ccfbd5b74b3672f632ef37d442d9
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-07-31 17:30:45 +0100

    Passed all 8 test 1. Created new empty array; 2. cycled through all the original array items 3. tested the arguments against each array item 4. pushed in the new array the array items not included in the arguments

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+/* const removeFromArray2 = function(arr, item) {
+    let array = Array.from(arr)
+    for (i=0; i < arr.length; i++) {
+        if (String(item) == String(arr[i])) {
+            console.log(true);
+            array.splice(i, 1);
+        }
+    }
+    return array
+}; */
+
+const removeFromArray = function(arr, ...args) {
+    let array = [];
+
+    for (const arrItem of arr) {
+        if (!args.includes(arrItem)) {
+            array.push(arrItem);
+        }
+    }
+    return array
 };
+
+console.table(removeFromArray([1,2,3,3,5],2,3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
