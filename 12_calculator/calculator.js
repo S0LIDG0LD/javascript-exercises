3e2adb18a6a30273d2d70a1da535eb69a4a611ed
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-05 11:04:34 +0100

    Added the factor function using a for loop;

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -22,12 +22,18 @@ const multiply = function(arr) {
   return arr.reduce((total, item) => (total *= item), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factor = 1;
+  if (a > 1) {
+    for (i = 2; i <= a; i++ ) {
+      factor *= i;
+    }
+  }
+	return factor
 };
 
 // Do not edit below this line
