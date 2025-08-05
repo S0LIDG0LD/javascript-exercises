acf6f4a833795316755ca9655ffa1f63206b8088
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-05 13:30:34 +0100

    Used ifs for base cases: negatives, zero and 1. Used a for loop to compute the F2 to Fn

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 0) {
+        return 0;
+    }
+    else if (num == 1) {
+        return 1;
+    } 
+    else {
+        let arr = [];
+            arr[0] = 0;
+            arr[1] = 1;
+        for (i = 2; i < num; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        let lastItem = arr.length;
+        return (arr[lastItem - 2] + arr[lastItem - 1]);
+    }
 };
 
 // Do not edit below this line
